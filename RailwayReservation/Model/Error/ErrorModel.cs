2e77b8e77e967e7c31f4b0767473f9f5f7322613
kaxxsh <kameshwaran2002@gmail.com>
2024-05-30 20:59:26 +0530

    adding the error model

--- RailwayReservation/Model/Error/ErrorModel.cs
+++ RailwayReservation/Model/Error/ErrorModel.cs
@@ -2,5 +2,14 @@
 {
     public class ErrorModel
     {
+        public int StatusCode { get; set; }
+
+        public string Message { get; set; }
+
+        public ErrorModel(int statusCode, string message)
+        {
+            StatusCode = statusCode;
+            Message = message;
+        }
     }
 }
